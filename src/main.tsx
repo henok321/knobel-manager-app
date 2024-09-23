5c2d44995465d7fc4664a7a82738407ba91971cc
Hendrik Brinkmann <henok321@gmail.com>
2024-09-23 20:17:25 +0200

    setup redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import i18n from './i18n/i18n.ts';
 import { I18nextProvider } from 'react-i18next';
+import { Provider } from 'react-redux';
+import store from './store/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
+      </ChakraProvider>
+    </Provider>
   </StrictMode>,
 );
