75c7a674f71e4ed539d7c26d5352af439a125013
Hendrik Brinkmann <henok321@gmail.com>
2024-10-02 18:34:10 +0200

    firebase token axios config

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,23 @@
 import axios from 'axios';
+import { auth } from '../../firebase.ts';
 
 const apiClient = axios.create({
   baseURL: 'https://knobel-manager-service-566295896360.europe-west1.run.app',
 });
+
+axios.interceptors.request.use(
+  async (config) => {
+    config.headers.token = await auth.currentUser?.getIdToken();
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+type PingResponse = { message: string };
+
+export const ping = async () => {
+  const response = await apiClient.get<PingResponse>('/ping');
+  return response.data;
+};
 
 export default apiClient;
