d2fc5c7bc7baeb7ac8fd105c62c7d5ca2985d8cc
Hendrik Brinkmann <henok321@gmail.com>
2024-12-18 22:53:31 +0100

    stuff

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../auth/useAuth.ts';
+import { useTranslation } from 'react-i18next';
 
 const Login: React.FC = () => {
   const { user, loading, loginAction } = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { t } = useTranslation();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -30,9 +32,9 @@ const Login: React.FC = () => {
         onSubmit={handleSubmit}
         className="bg-white p-6 rounded shadow-md w-full max-w-sm"
       >
-        <h1 className="text-2xl font-bold mb-4">Login</h1>
+        <h1 className="text-2xl font-bold mb-4">{t('LOGIN_HEADING')}</h1>
         <div className="mb-4">
-          <label className="block mb-1">Email</label>
+          <label className="block mb-1">{t('LOGIN_EMAIL_INPUT_LABEL')}</label>
           <input
             type="email"
             className="border w-full p-2"
@@ -44,7 +46,9 @@ const Login: React.FC = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block mb-1">Password</label>
+          <label className="block mb-1">
+            {t('LOGIN_PASSWORD_INPUT_LABEL')}
+          </label>
           <input
             type="password"
             className="border w-full p-2"
@@ -59,7 +63,7 @@ const Login: React.FC = () => {
           type="submit"
           className="bg-blue-600 text-white w-full py-2 rounded hover:bg-blue-700"
         >
-          Log In
+          {t('LOGIN_SIGN_IN_BUTTON')}
         </button>
       </form>
     </div>
