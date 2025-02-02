9650f1103d0a7f90d119b964802abd2ea0b24f97
Hendrik Brinkmann <henok321@gmail.com>
2025-02-02 01:52:00 +0100

    visual: update team form styles

--- src/pages/home/TeamForm.tsx
+++ src/pages/home/TeamForm.tsx
@@ -23,17 +23,11 @@ const TeamForm = ({ isOpen, onClose, createTeam, teamSize }: TeamFormProps) => {
 
   const submit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    const team: TeamFormData = {
-      name: teamName,
-      members: players,
-    };
-    createTeam(team);
+    createTeam({ name: teamName, members: players });
     onClose();
   };
 
   const handleChangeTeamName = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
     setTeamName(e.target.value);
   };
 
@@ -41,7 +35,6 @@ const TeamForm = ({ isOpen, onClose, createTeam, teamSize }: TeamFormProps) => {
     index: number,
     e: ChangeEvent<HTMLInputElement>,
   ) => {
-    e.preventDefault();
     const newPlayers = [...players];
     newPlayers[index] = e.target.value;
     setPlayers(newPlayers);
@@ -68,60 +61,70 @@ const TeamForm = ({ isOpen, onClose, createTeam, teamSize }: TeamFormProps) => {
         <div>
           <label
             className="block text-sm font-medium text-gray-700"
-            htmlFor="name"
+            htmlFor="team-name"
           >
             {t('pages.home.team.form.label.name')}
           </label>
           <input
             required
             className="mt-1 block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-            id="name"
-            name="name"
+            id="team-name"
+            name="team-name"
             type="text"
             value={teamName}
             onChange={handleChangeTeamName}
           />
         </div>
+
         <div>
           <label
             className="block text-sm font-medium text-gray-700"
-            htmlFor="player"
+            htmlFor="players"
           >
             {t('pages.home.team.form.label.players')}
           </label>
-          {players.map((player, index) => (
-            <div key={index} className="flex items-center space-x-2">
-              <input
-                required
-                className="mt-1 block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                id="name"
-                name="name"
-                type="text"
-                value={player}
-                onChange={(e) => handleChangePlayer(index, e)}
-              />
-              <button
-                aria-label={t('pages.home.team.form.removePlayer')}
-                className="m-2 disabled:opacity-50"
-                disabled={players.length < 2}
-                onClick={() => removePlayer(index)}
-              >
-                <TrashIcon className="h-5 w-5" />
-              </button>
-            </div>
-          ))}
-          <button
-            aria-label={t('pages.home.team.form.addPlayer')}
-            className="mt-2 disabled:opacity-50"
-            disabled={players.length == teamSize}
-            onClick={() => addPlayer()}
-          >
-            <PlusIcon className="h-5 w-5" />
-          </button>
+          <div className="mt-1 space-y-4">
+            {players.map((player, index) => (
+              <div key={index} className="flex items-center">
+                <input
+                  required
+                  className="mt-1 block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                  id={`player-${index}`}
+                  name={`player-${index}`}
+                  type="text"
+                  value={player}
+                  onChange={(e) => handleChangePlayer(index, e)}
+                />
+                <button
+                  aria-label={t('pages.home.team.form.removePlayer')}
+                  className="ml-2 p-2 text-red-500 hover:text-red-600 disabled:opacity-50"
+                  disabled={players.length === 1}
+                  type="button"
+                  onClick={() => removePlayer(index)}
+                >
+                  <TrashIcon className="h-5 w-5" />
+                </button>
+              </div>
+            ))}
+
+            <button
+              aria-label={t('pages.home.team.form.addPlayer')}
+              className="flex items-center p-2 text-green-500 hover:text-green-600 disabled:opacity-50"
+              disabled={players.length >= teamSize}
+              type="button"
+              onClick={addPlayer}
+            >
+              <PlusIcon className="h-5 w-5" />
+              <span className="ml-2 text-sm">
+                {t('pages.home.team.form.addPlayer')}
+              </span>
+            </button>
+          </div>
         </div>
+
         <div className="flex justify-end">
           <button
-            className="inline-flex items-center justify-center rounded-md bg-blue-600 px-4 py-2 font-medium text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none disabled:bg-blue-300"
+            className="inline-flex items-center justify-center rounded-md bg-blue-600 px-4 py-2 font-medium text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:bg-blue-300"
             disabled={players.length !== teamSize}
             type="submit"
           >
