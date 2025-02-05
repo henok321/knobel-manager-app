190ffa2942627be938d3c7e2cddee677f238e6ea
Hendrik Brinkmann <henok321@gmail.com>
2025-02-05 16:32:19 +0100

    wip: display teams

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
 import TeamForm, { TeamFormData } from './TeamForm.tsx';
 import Layout from '../../components/Layout.tsx';
 import useGames from '../../slices/games/hooks.ts';
 import useTeams from '../../slices/teams/hooks.ts';
+import { selectTeamsByGameId } from '../../slices/teams/slice.ts';
 import { Game } from '../../slices/types.ts';
+import { RootState } from '../../store/store.ts';
 
 const Home = () => {
   const { t } = useTranslation();
   const { activeGame, fetchGames, status } = useGames();
   const [gameModalActive, setGameModalActive] = useState(false);
   const { createTeam } = useTeams();
+
+  const teams = useSelector((state: RootState) =>
+    activeGame?.id ? selectTeamsByGameId(state, activeGame.id) : [],
+  );
 
   const renderActiveGame = (game: Game) => (
     <p>
@@ -71,6 +78,19 @@ const Home = () => {
           <p>{t('pages.home.noActiveGame')}</p>
         </div>
       )}
+
+      <div>
+        {teams.map((team) => (
+          <div key={team.id}>
+            <h3>{team.name}</h3>
+            <ul>
+              {team.players.map((player) => (
+                <li key={player}>{player}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 };
