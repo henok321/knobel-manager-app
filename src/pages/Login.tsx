da37f593790aba19f3e473cbe4cf7418aa31961a
Hendrik Brinkmann <henok321@gmail.com>
2025-08-15 15:00:06 +0200

    visual: mobile view for login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Container,
   Flex,
   Group,
   Paper,
@@ -8,9 +9,10 @@ import {
   Stack,
   Text,
   TextInput,
+  useMantineTheme,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
-import { upperFirst } from '@mantine/hooks';
+import { upperFirst, useMediaQuery } from '@mantine/hooks';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Navigate } from 'react-router-dom';
@@ -25,6 +27,8 @@ const Login = (props: PaperProps) => {
   const [loginError, setLoginError] = useState<string | null>(null);
 
   const { t } = useTranslation();
+  const theme = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.md})`);
 
   const form = useForm<LoginData>({
     initialValues: {
@@ -66,68 +70,82 @@ const Login = (props: PaperProps) => {
     return <Navigate replace to="/" />;
   }
 
+  const renderLoginForm = () => (
+    <>
+      <Text fw={500} pb="md" size="lg">
+        {t('pages.login.heading')}
+      </Text>
+
+      <form
+        onSubmit={form.onSubmit((formData) => {
+          handleSubmit(formData);
+        })}
+      >
+        <Stack>
+          <TextInput
+            required
+            error={form.errors.email && 'Invalid email'}
+            label="Email"
+            placeholder={t('pages.login.fields.email.placeholder')}
+            radius="md"
+            value={form.values.email}
+            onChange={(event) =>
+              form.setFieldValue('email', event.currentTarget.value)
+            }
+          />
+
+          <PasswordInput
+            required
+            error={
+              form.errors.password &&
+              t('pages.login.fields.password.validationMessage')
+            }
+            label={t('pages.login.fields.password.label')}
+            placeholder={t('pages.login.fields.password.placeholder')}
+            radius="md"
+            value={form.values.password}
+            onChange={(event) =>
+              form.setFieldValue('password', event.currentTarget.value)
+            }
+          />
+        </Stack>
+
+        {loginError && (
+          <Text c="red" mt="md">
+            {loginError}
+          </Text>
+        )}
+
+        <Group justify="space-between" mt="xl">
+          <Button radius="xl" type="submit">
+            {upperFirst(t('pages.login.submit'))}
+          </Button>
+        </Group>
+      </form>
+    </>
+  );
+
   return (
     <Layout navbarActive={false}>
-      <Flex align="center" h="80vh" justify="center">
-        <Paper
-          withBorder
-          p="lg"
-          radius="md"
-          w={{ base: 320, sm: 400, md: 480, lg: 540, xl: 600 }}
-          {...props}
-        >
-          <Text fw={500} pb="md" size="lg">
-            {t('pages.login.heading')}
-          </Text>
-
-          <form
-            onSubmit={form.onSubmit((formData) => {
-              handleSubmit(formData);
-            })}
+      {isMobile ? (
+        <Flex align="center" h="40vh" justify="center">
+          <Container size="md" style={{ width: theme.breakpoints.md }}>
+            {renderLoginForm()}
+          </Container>
+        </Flex>
+      ) : (
+        <Flex align="center" h="80vh" justify="center">
+          <Paper
+            withBorder
+            p="lg"
+            radius="md"
+            w={{ md: '40rem', lg: '50rem' }}
+            {...props}
           >
-            <Stack>
-              <TextInput
-                required
-                error={form.errors.email && 'Invalid email'}
-                label="Email"
-                placeholder={t('pages.login.fields.email.placeholder')}
-                radius="md"
-                value={form.values.email}
-                onChange={(event) =>
-                  form.setFieldValue('email', event.currentTarget.value)
-                }
-              />
-
-              <PasswordInput
-                required
-                error={
-                  form.errors.password &&
-                  t('pages.login.fields.password.validationMessage')
-                }
-                label={t('pages.login.fields.password.label')}
-                placeholder={t('pages.login.fields.password.placeholder')}
-                radius="md"
-                value={form.values.password}
-                onChange={(event) =>
-                  form.setFieldValue('password', event.currentTarget.value)
-                }
-              />
-            </Stack>
-
-            {loginError && (
-              <Text c="red" mt="md">
-                {loginError}
-              </Text>
-            )}
-
-            <Group justify="space-between" mt="xl">
-              <Button radius="xl" type="submit">
-                {upperFirst(t('pages.login.submit'))}
-              </Button>
-            </Group>
-          </form>
-        </Paper>
-      </Flex>
+            {renderLoginForm()}
+          </Paper>{' '}
+        </Flex>
+      )}
     </Layout>
   );
 };
