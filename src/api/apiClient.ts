0c4277f8d223169ef3f2124982f4fce5dcb8b49a
Hendrik Brinkmann <henok321@gmail.com>
2025-10-19 18:08:21 +0200

    chore: avoid cors issues with vite proxy

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 import { auth as firebaseAuth } from '../auth/firebaseConfig';
 import {
+  Configuration,
   GamesApi,
   PlayersApi,
   ScoresApi,
   TablesApi,
   TeamsApi,
-  Configuration,
 } from '../generated';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.PROD ? import.meta.env.VITE_API_URL : '/api',
 });
 
 axiosInstance.interceptors.request.use(
@@ -24,13 +24,7 @@ axiosInstance.interceptors.request.use(
     Promise.reject(error instanceof Error ? error : new Error(String(error))),
 );
 
-const apiConfiguration = new Configuration({
-  basePath: import.meta.env.VITE_API_URL,
-  accessToken: async () => {
-    const idToken = await firebaseAuth.currentUser?.getIdToken();
-    return idToken || '';
-  },
-});
+const apiConfiguration = new Configuration();
 
 export const gamesApi = new GamesApi(
   apiConfiguration,
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(() => ({
-  plugins: [react()],
-  server: {},
-}));
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const apiUrl = env.VITE_API_URL || 'http://localhost:8080';
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: apiUrl,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
+});
