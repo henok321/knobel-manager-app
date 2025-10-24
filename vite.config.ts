7984effb2ac13ff88ab44f122fe73f9e282dbaf8
Hendrik Brinkmann <henok321@gmail.com>
2025-10-24 15:43:30 +0200

    chore: vite tls cert for local development

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const apiUrl = env.VITE_API_URL || 'http://localhost:8080';
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      mkcert({
+        hosts: ['localhost', '127.0.0.1'],
+        keyFileName: 'knobel-manager-app.key.pem',
+        certFileName: 'knobel-manager-app.cert.pem',
+      }),
+    ],
     server: {
       proxy: {
         '/api': {
