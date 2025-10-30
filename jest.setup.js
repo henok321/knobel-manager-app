ba00d6700b031f1ed7874dd5b34d67d34d157dec
Hendrik Brinkmann <henok321@gmail.com>
2025-10-30 20:35:08 +0100

    mock local storage

--- jest.setup.js
+++ jest.setup.js
@@ -17,6 +17,24 @@ class BroadcastChannelMock {
 }
 global.BroadcastChannel = BroadcastChannelMock;
 
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+
+global.localStorage = localStorageMock;
+
 global.importMeta = {
   env: {
     PROD: false,
@@ -38,5 +56,8 @@ jest.mock('./src/auth/firebaseConfig', () => ({
 const { server } = require('./src/test/setup/msw');
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  server.resetHandlers();
+  localStorage.clear();
+});
 afterAll(() => server.close());
